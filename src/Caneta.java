c47aba75c0e11e72ea08ca0012c80fa7d94a36d0
Jonas Martins <jonasllj_2012@live.com>
2020-07-22 15:04:34 -0300

    Projeto criado

---
+++ src/Caneta.java
@@ -0,0 +1,28 @@
+public class Caneta {
+	String modelo;
+	String cor;
+	double ponta;
+	int carga;
+	boolean tampada;
+	
+	void status() {
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("Uma caneta " + this.cor);
+		System.out.println("Ponta :" + this.ponta);
+		System.out.println("Carga :" + this.carga);
+		System.out.println("Está tampada? " + this.tampada);
+	}
+	void rabiscar() {
+		if(this.tampada == true) {
+			System.out.println("ERRO ! Nao posso rabiscar ");
+		}else {
+			System.out.println("Estou rabiscando");
+		}
+	}
+	void tampar() {
+		this.tampada = true;
+	}
+	void destampar() {
+		this.tampada = false;
+	}
+}
